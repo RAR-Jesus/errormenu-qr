46d2241d455451d6802ee5a755cf9c3fc584b340
jesus alberto <agito11752@gmail.com>
2021-11-28 10:57:19 -0600

    primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBhTrL5FDUn1LxZVIUF_bvkwAmkyAMKfXY",
+    authDomain: "miorden-qr.firebaseapp.com",
+    projectId: "miorden-qr",
+    storageBucket: "miorden-qr.appspot.com",
+    messagingSenderId: "385322312745",
+    appId: "1:385322312745:web:2c9295889a068371b32838",
+    measurementId: "G-114GLK6V4D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBhTrL5FDUn1LxZVIUF_bvkwAmkyAMKfXY",
+    authDomain: "miorden-qr.firebaseapp.com",
+    projectId: "miorden-qr",
+    storageBucket: "miorden-qr.appspot.com",
+    messagingSenderId: "385322312745",
+    appId: "1:385322312745:web:2c9295889a068371b32838",
+    measurementId: "G-114GLK6V4D"
+  }
 };
 
 /*
